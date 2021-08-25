5d1fd96cf56f16f98c5c3e333ea0a02ca56702c5
harrizaustin2000 <47132977+harrizaustin2000@users.noreply.github.com>
2021-08-25 13:39:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function display_Product_Info() {
+    const data = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json")
+    const data1 = await data.json();
+    console.log(data1);
+    data1.forEach((data2) => createDetail(data2));
+}
+
+function createDetail(data2) {
+    const product = document.createElement("div");
+    product.setAttribute("class", "container");
+
+
+
+
+    product.innerHTML = ` 
+    
+    <div class ="about">
+    <img class="productpic" src="${data2.api_featured_image}" width="200" height ="300">
+    <h1 span class ="heading">${data2.brand}<h1>
+    <h2 span class ="heading2">${data2.name}<h2>
+    <p><span class="descrip"> Description:<br><br></span>${data2.description}</p>
+    <p><spanclass="price"> Price:$ </span>${data2.price}</p>
+    <p span class ="buylink"><a href="${data2.product_link}">Buy now</a></p>
+    <hr> 
+    </div>`;
+    document.body.append(product);
+
+}
+display_Product_Info();
